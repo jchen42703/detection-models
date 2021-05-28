ec93462dfca22e7c30049867fe9aa7c6a8c3b290
jchen42703 <jchen42703@gmail.com>
2021-05-28 16:43:47 -0400

    Initialization

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # detection-models
- Object Detection Deep Learning Models
+
+The purpose of this repository is to make experimenting with traditional object detection models intuitive and modular. I made this library primarily for educational purposes. More production ready or R & D ready repositories are linked below.
+
+**Credits To:**
+
+- https://github.com/david8862/keras-YOLOv3-model-set
+- [pydarknet](https://github.com/cenkbircanoglu/yolov3-app)
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='detectionmodels',
+      version='0.0.1',
+      description='Library for models for generating random mouse movements',
+      url='',
+      author='Joseph Chen',
+      author_email='jchen42703@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'numpy',
+          'tensorflow',
+          'pandas',
+          'matplotlib'
+      ],
+      keywords=['deep learning'],
+      )
